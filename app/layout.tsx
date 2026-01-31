9a4b877e7ada326dc49d5ed4d88b4891c7f17585
Chris Shabsin <cshabsin@gmail.com>
2026-01-31 00:03:06 -0500

    Add Google Analytics (G-FZ09TVK2XZ)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body className="antialiased bg-gray-50 text-gray-900">
         {children}
       </body>
+      <GoogleAnalytics gaId="G-FZ09TVK2XZ" />
     </html>
   );
 }
